469adee85bb34da91b8337b06562d66f8419116f
googleknight <shubham.mathur2014@vit.ac.in>
2017-08-17 12:36:11 +0530

    Databses updated and added for IT,EC,Mech,Civil branches

--- scrap_campusinteraction/ITcompanieslist.py
+++ scrap_campusinteraction/ITcompanieslist.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import sqlite3
 import urllib.request
@@ -6,14 +7,14 @@ from bs4 import BeautifulSoup
 import re
 from dateutil import parser
 
-driver = webdriver.Firefox()
+driver = webdriver.Chrome()
 driver.get("http://www.campusinteraction.com/login")
 username = driver.find_element_by_name("userName")
 username.clear()
-username.send_keys("") #enter your username
+username.send_keys("") #username
 password = driver.find_element_by_name("password")
 password.clear()
-password.send_keys("")		#enter your password
+password.send_keys("")#password
 password.send_keys(Keys.RETURN)
 driver.implicitly_wait(5)
 driver.get("http://www.campusinteraction.com/viewStudentAllJobs.action?jobFetchLimit=0&isViewAllJobs=true")
@@ -23,6 +24,7 @@ soup = BeautifulSoup(html,"html.parser")
 soup=soup.table
 conn = sqlite3.connect('campus.sqlite')
 cur = conn.cursor()
+cur.execute('''CREATE TABLE IF NOT EXISTS `company` ( `name` TEXT , `type` TEXT, `salary` TEXT, `campusdate` DATE,`applydate` DATE, `selected` text, `shortlisted` text )''')
 jobtype=soup.findAll('a')
 jtype=list()
 jlink=list()
@@ -76,12 +78,24 @@ print('count:'+str(datacount))
 
 eligible=list()
 i=0
+selectedstudents={}
+shortlistedstudents={}
 print(len(jlink))
 for ele in jlink:
     driver.get("http://www.campusinteraction.com/"+ele)
     flag=0
     for line in driver.find_elements_by_xpath("//label[@class='jobProfileDetail']//li"):
         if line.text.strip()=="B.Tech - All Branches are eligible" or line.text.strip()=="B.Tech-Information Technology":
+            html = driver.page_source
+            soup = BeautifulSoup(html,"html.parser")
+            for link in soup.findAll('a'):
+                if link.has_attr("href"):
+                    matchobjselect=re.match(".*showCandidate\.action\?status=S&jobId.*",link['href'])
+                    matchobjshortlist = re.match(".*showCandidate\.action\?status=SL&jobId.*", link['href'])
+                    if matchobjselect:
+                        selectedstudents[cname[i]]=link.text.strip()
+                    if matchobjshortlist:
+                        shortlistedstudents[cname[i]]=link.text.strip()
             print(str(i)+' '+line.text+' '+cname[i])
             eligible.append(1)
             flag=1
@@ -93,11 +107,14 @@ print(str(len(eligible)))
 
 for i in range(0,datacount-1):
     if eligible[i]==1:
-        cur.execute('''INSERT OR IGNORE INTO company (name,type,salary,campusdate,applydate) VALUES ( ?,?,?,?,? )''', ( cname[i],jtype[i],salary[i],cday[i],applyday[i], ) )
-        print(cname[i])
+        selects=0
+        shortlist=0
+        if cname[i] in selectedstudents:
+            selects=selectedstudents[cname[i]]
+        if cname[i] in shortlistedstudents:
+            shortlist=shortlistedstudents[cname[i]]
+        cur.execute('''INSERT OR IGNORE INTO company (name,type,salary,campusdate,applydate,selected,shortlisted) VALUES ( ?,?,?,?,?,?,? )''', ( cname[i],jtype[i],salary[i],cday[i],applyday[i],selects,shortlist ) )
 conn.commit()
-
-
 driver.execute_script("dologout()")
 driver.implicitly_wait(5)
 driver.close()
